8273475b48198e6efa61f64dcf9ddf868427da37
MiniyahCrouch <mcrouch7@students.kennesaw.edu>
2026-04-23 13:53:57 -0400

    Implement task addition feature
    Added functionality to add tasks button

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ root = tk.Tk()
 root.title("Task Wrangler")
 root.geometry("400x400")
 
+def add_task():
+    task = task_entry.get()
+    if task:
+        task_listbox.insert(tk.END, task)
+        task_entry.delete(0, tk.END)
+
 task_entry = tk.Entry(root, width=30)
 task_entry.pack(pady=10)
 
-add_button = tk.Button(root, text="Add Task")
+add_button = tk.Button(root, text="Add Task", command=add_task)
 add_button.pack()
+
+task_listbox = tk.Listbox(root, width=40, height=10)
+task_listbox.pack(pady=10)
 
 root.mainloop()
