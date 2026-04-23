a56c996e0fb451b8fcf6267d95b3241f1224ca1e
MiniyahCrouch <mcrouch7@students.kennesaw.edu>
2026-04-23 13:34:53 -0400

    Add task entry and button to main window

--- main.py
+++ main.py
@@ -4,4 +4,10 @@ root = tk.Tk()
 root.title("Task Wrangler")
 root.geometry("400x400")
 
+task_entry = tk.Entry(root, width=30)
+task_entry.pack(pady=10)
+
+add_button = tk.Button(root, text="Add Task")
+add_button.pack()
+
 root.mainloop()
